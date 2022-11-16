702b9c4ef76eb20c82ed031ed496f6d7e611a7eb
Durgesh Soni <durgeshsoni1082003@gmail.com>
2022-11-16 22:46:52 +0530

    added css for deposit

--- src/components/Dashboard/Deposit.jsx
+++ src/components/Dashboard/Deposit.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import Header from './Header'
-import './styles/deposit.css'
-import {BiArrowBack} from 'react-icons/bi'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Header from "./Header";
+import "./styles/deposit.css";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
-
- 
 const Deposit = () => {
   return (
     <>
-    <Header/>
-    <div className="container">
-      <div className="box_container Deposit">
-        <Link to='/dashboard'>  <span id='arrow'> <BiArrowBack/> </span></Link> 
-        <h1>Deposit on Your Account</h1>
-        <label >Enter Amount</label> 
-        <input type="text" name="amount" id="amount" placeholder='Enter amount' /> <br/>
-        <button className='drabtn'> Deposit </button>
-     
-
-        
+      <Header />
+      <div className="container">
+        <div className="box_container Deposit">
+        <Link to="/dashboard">
+            {" "}
+            <span id="arrow">
+              {" "}
+              <BiArrowBack />{" "}
+            </span>
+          </Link>
+          <div className="content">
+         
+          <h1>Deposit on Your Account</h1>
+          <label>Enter Amount</label>
+          <input
+            type="text"
+            name="amount"
+            id="amount"
+            placeholder="Enter amount"
+          />{" "}
+          <br />
+          <button className="drabtn"> Deposit </button>
+          </div>
+        </div>
       </div>
-      </div>   
-
-    
-    
     </>
-  )
-}
+  );
+};
 
-export default Deposit
+export default Deposit;
